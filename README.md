6c61b50adf6bcb33fd012a89e5569596ff1c8d31
Unknown <120293781+XYUSTAR@users.noreply.github.com>
2023-05-18 13:05:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# reader-replit
+
+## 鸣谢
+- [hectorqin/reader](https://github.com/hectorqin/reader)
+- [andbruibm/reader-replit](https://github.com/andbruibm/reader-replit)
+
+## 在 Replit 免费服务上部署 阅读3服务器版
+
+前往 replit.com 注册账户，然后点击下方按钮
+
+<a href="https://repl.it/github/wy580477/reader-replit">
+  <img alt="Run on Repl.it" src="https://repl.it/badge/github/andbruibm/reader-replit" style="height: 40px; width: 190px;" />
+</a>
+
+再点击 Import from Github。
+
+多用户设置方法：
+
+![image](https://user-images.githubusercontent.com/98247050/205486934-e2644eff-465d-4f4c-b0ac-68bb33e80cd7.png)
+
+点击左侧Secrets，在右侧选项卡分别设置 secureKey（管理密码）和 inviteCode（注册邀请码）变量。
+如不需要多用户，可跳过此步。
+
+然后点击网页上方 Run，稍等片刻即部署完成。
+
+![image](https://user-images.githubusercontent.com/98247050/205486877-36558e55-b423-4fe3-8477-3ddbe6bd6c09.png)
---
+++ main.sh
@@ -0,0 +1,18 @@
+FileName=$(basename reader*.jar)
+declare -r FileName
+tag2=$(wget -qO- -t1 -T2 "https://api.github.com/repos/hectorqin/reader/releases/latest" | grep "tag_name" | head -n 1 | awk -F "v" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+
+if [[ ! "${FileName}" =~ "${tag2}" ]] && [ "${tag2}" != "" ]; then
+    tag1=$(wget -qO- -t1 -T2 "https://api.github.com/repos/hectorqin/reader/releases/latest" | jq -r '.tag_name')
+    wget https://github.com/hectorqin/reader/releases/download/${tag1}/reader-pro-${tag2}.jar
+    EXIT_CODE=$?
+    if [ "${FileName}" != reader\*.jar ] && [ "${EXIT_CODE}" -eq 0 ]; then
+        rm -rf ${FileName}
+    fi
+fi
+
+if [ "${secureKey}" != "" ]; then
+    java -jar reader*.jar --reader.app.secure=true --reader.app.secureKey=${secureKey} --reader.app.inviteCode=${inviteCode}
+else
+    java -jar reader*.jar
+fi
